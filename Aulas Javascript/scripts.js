243171514a9ac97938f15e0022466cd62974a46a
Joao Vitor <joaovitorsneo@gmail.com>
2023-08-03 02:20:10 +0000

    commit

---
+++ Aulas Javascript/scripts.js
@@ -0,0 +1,22 @@
+console.log("Teste")
+var  a = 10;
+var b = 20;
+var total =  + b;
+console.log(total);
+
+var nomePessoa = "Vitor";
+console.log(nomePessoa);
+
+let book = 20;
+
+/*código em java
+System.out.println("Teste");
+Int a = 10;
+Int b = 20;
+Int total = a + b;
+System.out.println(total);
+
+String nomePessoa = "Vitor";
+System.out.println(nomePessoa);
+*/
+
---
+++ data_types.js
@@ -0,0 +1,21 @@
+var nome = "Vitor"
+console.log(typeof(nome));
+if(typeof(nome) == "string"){
+    
+    console.log(nome.toLocaleUpperCase());
+}else{
+    console.log("Você precisa usar uma string")
+}
+
+//OPERADORES
+
+var n = 3;
+//n = n -10;
+n *= 10;
+console.log(n);
+
+
+
+var x = 10;
+x++
+x--
